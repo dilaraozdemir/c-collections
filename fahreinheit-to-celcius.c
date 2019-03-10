fd0ec4042305453c447446c42a3d968878cde5d7
Dilara Özdemir <42623884+rhaenessa@users.noreply.github.com>
2019-03-10 15:43:38 +0300

    Add files via upload

---
+++ fahreinheit-to-celcius.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//5. Convert Temperature from Fahrenheit (?) to Celsius (?).
+int main(){
+	float f,c;
+	
+	printf("Enter the value: ");
+	scanf("%f",&f);
+	
+	c =5.0/9.0*(f-32);
+	printf("%f",c);
+	
+}
